95d1949205676bc037d1db6b485676865ff75928
shriyashshukla <harishukla797@gmail.com>
2024-05-13 14:28:10 +0530

    ...

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
       <Link className="text-white font-bold" href={"/"}>
-        GTCoding.
+      list of topics
       </Link>
       <Link className="bg-white p-2" href={"/addTopic"}>
         Add Topic
